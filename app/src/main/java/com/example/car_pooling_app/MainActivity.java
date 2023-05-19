9a651be0e2f4253ac9ced7c778f2fcb221a332ee
zeyad-ahmed-samer <zeyadbasha1@hotmail.com>
2023-05-19 20:25:51 +0200

    project setup

---
+++ app/src/main/java/com/example/car_pooling_app/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.car_pooling_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+    }
+
+
+
+
+
+
+}
