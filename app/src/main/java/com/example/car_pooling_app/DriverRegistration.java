f7ff777f5a2f2f34ab59b3c9301eaeca846e1015
zeyad-ahmed-samer <zeyadbasha1@hotmail.com>
2023-05-24 14:12:00 +0200

    added signin and signup functionality

--- app/src/main/java/com/example/car_pooling_app/DriverRegistration.java
+++ app/src/main/java/com/example/car_pooling_app/DriverRegistration.java
@@ -1,12 +1,20 @@
 package com.example.car_pooling_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class DriverRegistration extends AppCompatActivity {
 
@@ -23,20 +31,20 @@ public class DriverRegistration extends AppCompatActivity {
     Button registeringButtonStatus;
     Button signButton;
     Boolean isSigningIn=false;
-
+    FirebaseAuth firebaseAuth=FirebaseAuth.getInstance();
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_registration);
-        usernameEditText=findViewById(R.id.username);
-        emailEditText=findViewById(R.id.email);
-        passwordEditText=findViewById(R.id.password);
-        plateNumberEditText=findViewById(R.id.plateNumber);
-        carTypeEditText=findViewById(R.id.carType);
+        usernameEditText=(EditText) findViewById(R.id.username);
+        emailEditText=(EditText) findViewById(R.id.email);
+        passwordEditText=(EditText) findViewById(R.id.password);
+        plateNumberEditText=(EditText) findViewById(R.id.plateNumber);
+        carTypeEditText=(EditText) findViewById(R.id.carType);
         registeringButtonStatus=findViewById(R.id.registerButtonStatus);
-        phoneNumberEditText=findViewById(R.id.phone);
+        phoneNumberEditText=(EditText) findViewById(R.id.phone);
         screenTitle=findViewById(R.id.screenTitle);
 
 
@@ -55,6 +63,33 @@ public class DriverRegistration extends AppCompatActivity {
                     renderRegisterScreen();
 
                 }
+            }
+        });
+
+
+        signButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email=emailEditText.getText().toString();
+                String username=usernameEditText.getText().toString();
+                String password=passwordEditText.getText().toString();
+                String phone=phoneNumberEditText.getText().toString();
+                String carType=carTypeEditText.getText().toString();
+                String plateNumbet=plateNumberEditText.getText().toString();
+
+                if (isSigningIn) {
+
+                    Log.d("debugging", "email "+email + password);
+
+                    logIn(email, password);
+                }else {
+                    signUp(email,password);
+                }
+
+
+
+
+
             }
         });
 
@@ -95,11 +130,52 @@ public class DriverRegistration extends AppCompatActivity {
 
 
 
+    }
+
+
+
+    private void signUp(String email, String password){
+        firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if(task.isSuccessful()) {
+                    Toast.makeText(DriverRegistration.this, "Signed Up successfully", Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(DriverRegistration.this, "Error in Signing Up", Toast.LENGTH_LONG).show();
+                }
+
+            }
+
+
+
+        });
+
+    }
+
+
+    private void logIn(String email,String password){
+
+
+        firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+          @Override
+          public void onComplete(@NonNull Task<AuthResult> task) {
+              if(task.isSuccessful()) {
+                  Toast.makeText(DriverRegistration.this, "logged in successfully", Toast.LENGTH_LONG).show();
+              }else{
+                  Toast.makeText(DriverRegistration.this, "Error in Signing in", Toast.LENGTH_LONG).show();
+              }
+
+          }
+      }
+
+        );
 
 
 
 
     }
+
 
 
 
--- app/src/main/java/com/example/car_pooling_app/RiderRegistration.java
+++ app/src/main/java/com/example/car_pooling_app/RiderRegistration.java
@@ -1,12 +1,20 @@
 package com.example.car_pooling_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RiderRegistration extends AppCompatActivity {
 
@@ -21,6 +29,10 @@ public class RiderRegistration extends AppCompatActivity {
     Button registeringButtonStatus;
     Button signButton;
     Boolean isSigningIn=false;
+    
+    
+    
+    private FirebaseAuth firebaseAuth=FirebaseAuth.getInstance();
 
 
 
@@ -31,26 +43,26 @@ public class RiderRegistration extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rider_registration);
 
-
-        usernameEditText=findViewById(R.id.username);
-        emailEditText=findViewById(R.id.email);
-        passwordEditText=findViewById(R.id.password);
-
+        usernameEditText=(EditText) findViewById(R.id.username);
+        emailEditText=(EditText) findViewById(R.id.email);
+        passwordEditText=(EditText) findViewById(R.id.password);
         registeringButtonStatus=findViewById(R.id.registerButtonStatus);
-        phoneNumberEditText=findViewById(R.id.phone);
+        phoneNumberEditText=(EditText) findViewById(R.id.phone);
         screenTitle=findViewById(R.id.screenTitle);
-
         signButton=findViewById(R.id.signButton);
 
 
 
+
         registeringButtonStatus.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
                 isSigningIn=!isSigningIn;
 
                 if(isSigningIn){
                     renderLoginScreen();
+
 
                 }else{
                     renderRegisterScreen();
@@ -59,7 +71,41 @@ public class RiderRegistration extends AppCompatActivity {
             }
         });
 
+
+
+        signButton.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View view) {
+                String email=emailEditText.getText().toString();
+                String username=usernameEditText.getText().toString();
+                String password=passwordEditText.getText().toString();
+                String phone=phoneNumberEditText.getText().toString();
+
+
+
+                if (isSigningIn) {
+
+                    Log.d("debugging", "email "+email + password);
+
+                    logIn(email, password);
+                }else {
+                    signUp(email,password);
+                }
+            }
+        });
+
+
+
+
     }
+
+
+
+
+
+
 
 
 
@@ -86,6 +132,49 @@ public class RiderRegistration extends AppCompatActivity {
         phoneNumberEditText.setVisibility(View.VISIBLE);
         usernameEditText.setVisibility(View.VISIBLE);
 
+
+
+
+
+    }
+
+
+    private void signUp(String email, String password){
+        firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if(task.isSuccessful()) {
+                    Toast.makeText(RiderRegistration.this, "Signed Up successfully", Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(RiderRegistration.this, "Error in Signing Up", Toast.LENGTH_LONG).show();
+                }
+
+            }
+
+
+
+        });
+
+    }
+
+
+    private void logIn(String email,String password){
+
+
+        firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()) {
+                    Toast.makeText(RiderRegistration.this, "logged in successfully", Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(RiderRegistration.this, "Error in Signing in", Toast.LENGTH_LONG).show();
+                }
+
+            }
+            }
+
+        );
 
 
 
