2e210db770836ebd6040e6b80093cf3aaae0a4db
zeyad-ahmed-samer <zeyadbasha1@hotmail.com>
2023-05-28 15:09:28 +0200

    transferred object over shared preference

--- app/src/main/java/com/example/car_pooling_app/IncomingRequestsActivity.java
+++ app/src/main/java/com/example/car_pooling_app/IncomingRequestsActivity.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.widget.TextView;
 
 import com.example.car_pooling_app.models.Driver;
+import com.google.gson.Gson;
 //import com.google.gson.Gson;
 
 public class IncomingRequestsActivity extends AppCompatActivity {
@@ -22,11 +23,14 @@ public class IncomingRequestsActivity extends AppCompatActivity {
 
         SharedPreferences sPreferences = getSharedPreferences("sPref",Context.MODE_PRIVATE);
         String nameSharedPref = sPreferences.getString("MyObject",null);
+        Gson gson = new Gson();
+        Driver driver = gson.fromJson(nameSharedPref,Driver.class);
+
+
         if(nameSharedPref != null)
         {
-            testing.setText(nameSharedPref);
+            testing.setText(driver.getPhoneNumber());
         }
-
         else {
             testing.setText("Error parsing data from shared preference");
         }
